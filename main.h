aa038b67fe27ec02c6240d7af464c2845ab367c1
mamado143 <midodahir123@gmail.com>
2023-11-22 07:22:33 -0800

    update main.h

--- main.h
+++ main.h
@@ -3,11 +3,10 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-// Function prototype for _printf
 int _printf(const char *format, ...);
 int _putchar(char c);
 void print_char(va_list args, int *count);
 void print_str(va_list args, int *count);
 void print_percent(int *count);
 void print_integer(va_list args, int *count);
-#endif  // PRINTF_H
+#endif
